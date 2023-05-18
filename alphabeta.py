2e9cf7eaf957b26a0c5fc9081fbe127f80f24f73
Garic152 <erictheiler15@gmail.com>
2023-05-18 20:46:50 +0200

    Continued the alpha beta minimax search function

--- alphabeta.py
+++ alphabeta.py
@@ -1,7 +1,8 @@
 import time
 
-import evaluate
+import evaluate as evaluate
 import main_copy as main
+from main_copy import Moves
 
 def minimax(allowed_time: int, depth: int, board: main.Board):
     #define for which side minimax should run
@@ -11,7 +12,7 @@ def minimax(allowed_time: int, depth: int, board: main.Board):
     timer = time.time()
 
     #start generating the first moves
-    moves = main.Moves()
+    moves = Moves()
     main.generate_move(moves, board)
     
     #if lenth is zero the game is over or something went wrong
@@ -29,10 +30,44 @@ def minimax(allowed_time: int, depth: int, board: main.Board):
             return 0
 
         value = alpha_beta(depth - 1, not maximize, board)
-
         board.undo_move()
+
     return moves[0]
 
 
 def alpha_beta(depth: int, maximize: bool, board):
+    #look if the alpha_beta function should optimize for white or black,
+    #then initialize with the corresponding initial values of -inf and inf for alpha and beta
+    if maximize:
+        return alpha_beta_max(-float("inf"), float("inf"), depth, board)
+    else:
+        return alpha_beta_min(-float("inf"), float("inf"), depth, board)
+
+
+def alpha_beta_max(alpha, beta, depth, board):
+    #check if depth is 0, if yes evaluate and return the current board
+    if depth == 0:
+        #return evaluate(board)
+        pass
+
+    moves = Moves()
+    main.generate_move(moves, board)
+
+    for move in moves:
+        #copy move, make it and then pass it into min
+        board.copy_move()
+
+        if not main.make_move(move, board):
+            return 0
+
+        value = alpha_beta_min(alpha, beta, depth - 1, board)
+        board.undo_move()
+
+    if value >= beta:
+        return beta
+    elif value > alpha:
+        alpha = value
+    return alpha
+
+def alpha_beta_min(alpha, beta, depth, board):
     pass
